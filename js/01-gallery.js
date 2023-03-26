5a3ac1f7753f9a0ec2ba5a93e89c4d90478ff675
Valeriy <valeriy.all@gmail.com>
2023-03-26 19:06:27 +0300

    1. Image gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,1 +1,48 @@
 import { galleryItems } from "./gallery-items.js";
+
+const galleryReference = document.querySelector(".gallery");
+
+function createGallery(items) {
+  items.map((item) => {
+    const markup = `<div class="gallery__item">
+                      <a class="gallery__link" href="${item.original}">
+                        <img
+                          class="gallery__image"
+                          src="${item.preview}"
+                          data-source="${item.original}"
+                          alt="${item.description}"
+                        />
+                      </a>
+                    </div>`;
+
+    galleryReference.insertAdjacentHTML("beforeend", markup);
+  });
+}
+createGallery(galleryItems);
+
+galleryReference.addEventListener("click", selectImage);
+
+function selectImage(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImage = event.target.dataset.source;
+  const modal = basicLightbox.create(
+    `<img width="1280" height="854" src="${largeImage}">`
+  );
+  modal.show();
+
+  document.addEventListener("keydown", handleEscPress);
+
+  function handleEscPress(event) {
+    if (event.key === "Escape") {
+      modal.close();
+      document.removeEventListener("keydown", handleEscPress);
+    }
+  }
+}
+
+// console.log(basicLightbox)
